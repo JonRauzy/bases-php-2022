22362f896c0bb53e03afbf2c314340b0345dcc8f
JonRauzy <jonathan.rauzy@gmail.com>
2022-10-14 16:15:14 +0200

    interface

--- 12-conditions.php
+++ 12-conditions.php
@@ -19,13 +19,49 @@
     }
     echo "<p>if(isset)</p>";
 
-    if(isset($nb) && $nb < 10 && $nb != 10){ //verifie si elle est initialisée
+    if(isset($nb) && $nb < 10 && $nb != 10){ // isset() verifie si la variable est initialisée
         echo "$nb existe et il est plus petit que 10";
     }else{
         echo "$nb existe et il est plus grand que 10";
     }
 
     ?>
-    <p></p>
+    <p>les conditions peuvent être moulte complexe oui <a href="https://www.php.net/manual/en/language.operators.logical.php"> check ici fiss</a></p>
+    <h2>else : SINON</h2>
+    <p>ça execute le code si le if qui précéde est false</p>
+    <?php
+    $temp = mt_rand(-20, 30);
+    echo "<h4>Il fait $temp degres</h4>";
+
+    if($temp <= 0){
+        echo "y gele";
+    }else{
+        echo "y gele po";
+    }
+
+    echo "<h2>Sinon si - ifelse</h2><br>";
+    echo "<p>tu execute une verification supplémentaire si le if et false (ou un autre elsif)</p>";
+
+    $temp = mt_rand(-20, 30);
+    echo "<h4>Il fait $temp degres</h4>";
+
+    if($temp <= -10){
+        echo "y caille! ";
+    }elseif($temp <= 0){
+        echo "y gele!  ";
+    }elseif($temp <= 8){
+        echo "y fe froid! ";
+    }elseif($temp <= 12){
+        echo "y fe tiede! ";
+    }elseif($temp <= 20){
+        echo "y fa bon! ";
+    }else{
+        echo "y fa chaud! ";
+    }    
+
+    
+    ?>
+
+
 </body>
 </html>
---
+++ 13-interface-utilisateur-if.php
@@ -0,0 +1,35 @@
+<?php
+// debogage de la variable post
+$_POST;
+var_dump($_POST);
+
+//si il existe la variable $_POST nommé temperature (utilisateur isset) dans notre cas $_POST['temperature'] existe si on a envoyé le formulaire
+
+if(isset($_POST['temperature'])){
+    echo "bite";
+}
+
+$temp = (int)$_POST['temperature'];
+var_dump($temp);
+
+// on va créer une nouvelle variable temporaire, là on va l'appeler $temp = (int)$_POST['temperture'], si ça fonctionne ça donne un numérique , en cas d'échec ça nous donne 0
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Températures</title>
+</head>
+<body>
+    <h1>Température</h1>
+    <p>Pis y'a aussi les super global variable <a href="https://www.php.net/manual/en/language.variables.superglobals.php" target="_blank">check ici fiss</a> </p>
+    <form name="formulaire" action="" method="POST">
+        <p>Bonjour qu'elle est la température du dehors</p>
+        <input type="number" max="40" min="-30" name="temperature" placeholder="donne la temperature" style="width: 100px;"><br>
+        <input type="submit" value="Envoyer" style="width: 75px;">
+    </form>
+</body>
+</html>
