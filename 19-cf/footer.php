4f2065d72872cd6b8f8a123007a1e36345b5fd59
JonRauzy <jonathan.rauzy@gmail.com>
2022-10-26 09:31:24 +0200

    different

--- 19-cf/footer.php
+++ 19-cf/footer.php
@@ -1,3 +1,7 @@
 <footer style="text-align:center; background-color: black; color: white; height: 15vh;">
     Copyright du cul <?= date("Y") ?>
 </footer>
+
+</body>
+
+</html>
--- 19-cf/menu.php
+++ 19-cf/menu.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>SataN</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
 <!-- Responsive navbar-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
