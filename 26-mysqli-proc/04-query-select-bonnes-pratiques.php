a7c5ef873a886aaf219867c2aa6326ea8a40e256
JonRauzy <jonathan.rauzy@gmail.com>
2022-11-23 16:26:24 +0100

    bonne soirée

---
+++ 26-mysqli-proc/04-query-select-bonnes-pratiques.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'config.php';
+
+try{
+    $db = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
+    mysqli_set_charset($db, DB_CHARSET);
+}catch(Exception $e){
+    
+    exit(utf8_encode($e->getMessage()));
+}
+
+$sql = "SELECT * FROM statistiques ORDER BY nom AsC";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BONNES PRATIQUES</title>
+</head>
+<body>
+    <h1></h1>
+
+<?php
+mysqli_close($db)
+?>
+</body>
+</html>
