56c80b05c8c7e5f27c75fcae1ff58d1382fef5fd
JonRauzy <jonathan.rauzy@gmail.com>
2022-11-25 09:08:01 +0100

    triche

--- 26-mysqli-proc/05-query-select-encore.php
+++ 26-mysqli-proc/05-query-select-encore.php
@@ -1,46 +1,81 @@
 <?php
-require_once 'config.php';
-require 'fonction.php';
+require_once "config.php";
+
+require "functions.php";
+
+// connexion avec try catch
 
 try {
-    $db = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
-    mysqli_set_charset($db, DB_CHARSET);
+    // création d'une variable contenant la connexion
+    $db = @mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
 
-} catch (Exception $e) {
-    echo utf8_encode($e->getMessage());
+    // si erreur
+    if (mysqli_connect_errno()) {
+        // sortie et affichage du numéro de l'erreur et son message
+        die("Erreur numéro : ".mysqli_connect_errno()." Message : ".mysqli_connect_error());
+    }
+    // mettre le charset à la connexion
+    mysqli_set_charset($db, DB_CHARSET);
+}catch(Exception $e){
+
+    die("Numéro : ".$e->getCode()." | ".$e->getMessage());
+
+}
+// sélectionne tout depuis la table stat ordonnée par nom ascendant
+$sql = "SELECT * FROM statistiques #WHERE ID =700 
+        ORDER BY nom ASC";
+
+try {
+    // requête
+    $query = mysqli_query($db, $sql) or die("Erreur numéro : ".mysqli_errno($db)." Message : ".mysqli_error($db));
+}catch(Exception $e){
+
+    die("Numéro : ".$e->getCode()." | ".$e->getMessage());
+
 }
 
-$sql = "SELECT * FROM statistiques ORDER BY `nom` ASC";
+// comptons le nombre de résultats
+$nb = mysqli_num_rows($query);
 
-$requete = mysqli_query($db,$sql);
+// si on a pas de résultats (0)
+if(empty($nb)){
+    // création du message
+    $affiche = "Pas encore de pays dans notre programme";
 
-$nbPays = mysqli_num_rows($requete);
+}else{
+    // mettre les résultats dans une variable de type tableau pour faire un foreach dans la vue
+    $datas = mysqli_fetch_all($query,MYSQLI_ASSOC);
+}
+
+// bonnes pratiques on va vider les résultats
+mysqli_free_result($query);
+// bonnes pratiques on va fermer la connexion
+mysqli_close($db);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BONNES PRATIQUES</title>
+    <title>Accueil</title>
 </head>
-
 <body>
-    <h1>yanis</h1>
+    <h1>Accueil</h1>
+    <h2>Nombre de pays : <?=$nb?></h2>
     <?php
-    if(isset($nbPays)):   
-        while ($item = mysqli_fetch_assoc($requete)) : 
-        ?>
-            <p><?= $item['nom']?> - <?= perMillion($item['population']) ?></p>
-        <?php
-
-    endwhile;
-endif;
-
+    if(isset($affiche)): 
+    ?>
+    <h3><?=$affiche?></h3>
+    <?php
+    else:
+        foreach($datas as $fifa):
+    ?>
+    <p><strong><?=$fifa['nom']?></strong> <?=perMillion($fifa['population'])?> d' Habitants</p>
+    <?php
+        endforeach;
+    endif;
     ?>
 </body>
-
 </html>
