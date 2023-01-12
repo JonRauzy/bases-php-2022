b6808efc1189e17a009efcd9293d373617f47553
JonRauzy <jonathan.rauzy@gmail.com>
2023-01-12 08:58:05 +0100

    pk

--- 28-mysqli-crud/public/index.php
+++ 28-mysqli-crud/public/index.php
@@ -1,11 +1,12 @@
 <?php
 require_once '../config.php';
 
+
 try {
     $db = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
     mysqli_set_charset($db, DB_CHARSET);
-}catch(Exception $e){
-    exit(mb_convert_encoding($e->getMessage(),'UTF-8','ISO-8859-1'));
+} catch (Exception $e) {
+    exit(mb_convert_encoding($e->getMessage(), 'UTF-8', 'ISO-8859-1'));
 }
 
 
@@ -13,13 +14,26 @@ $sql_user = "SELECT * FROM `users`";
 $query_user = mysqli_query($db, $sql_user) or die('erreur de problème');
 $resultat_user = mysqli_fetch_all($query_user, MYSQLI_ASSOC);
 
+
 $sql_rubrique = "SELECT * FROM `rubriques`";
 $query_rubrique = mysqli_query($db, $sql_rubrique);
 $resultat_rubrique = mysqli_fetch_all($query_rubrique, MYSQLI_ASSOC);
 
-$sql_article = "SELECT LEFT(articles.art_text, 60), articles.art_title, users.user_login, users.idusers FROM articles JOIN users ON articles.users_idusers = users.idusers";
+
+$sql_article = "SELECT a.*, u.user_login AS login, u.idusers
+                FROM articles a 
+                INNER JOIN users u 
+                ON a.users_idusers = u.idusers";
 $query_article = mysqli_query($db, $sql_article);
 $resultat_article = mysqli_fetch_all($query_article, MYSQLI_ASSOC);
 
 
-var_dump($resultat_article);
+$sql_3 = "SELECT a.idarticles, a.art_title, SUBSTR(a.art_text, 1, 240) AS art_text, a.art_date, u.user_login, u.idusers 
+        FROM `articles` a
+        LEFT JOIN users u
+        ON u.idusers = a.users_idusers";
+$query_3 = mysqli_query($db, $sql_3);
+$resultat_3 = mysqli_fetch_all($query_3, MYSQLI_ASSOC);
+
+
+var_dump($resultat_3);
