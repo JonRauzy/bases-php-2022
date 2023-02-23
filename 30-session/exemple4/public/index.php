0734e7692d3a77710469e0bb9f3aef46340d3dd9
JonRauzy <jonathan.rauzy@gmail.com>
2023-02-23 16:26:33 +0100

    exe4.2

--- 30-session/exemple4/public/index.php
+++ 30-session/exemple4/public/index.php
@@ -3,3 +3,15 @@
 session_start();
 
 require_once '../config.php';
+
+
+try{
+ 
+    $db = mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
+
+    mysqli_set_charset($db, DB_CHARSET);
+
+}catch(Exception $e){
+
+    exit(mb_convert_encoding($e->getMessage(),'UTF-8','ISO-8859-1'));
+}
